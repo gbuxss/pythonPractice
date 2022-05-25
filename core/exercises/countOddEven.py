4f09c8eed5e1489477c2fa685bc670f0bced2ece
gbuxss <jeevandhamala095@gmail.com>
2022-05-25 17:02:10 -0500

    count odd even added

---
+++ core/exercises/countOddEven.py
@@ -0,0 +1,12 @@
+# write a program count odd/ even number in a list
+
+numbers_list = [5, 3, 5, 3, 7, 8, 9, 4, 6]
+odd = 0
+even = 0
+for num in numbers_list:
+    if num % 2 == 0:
+        even += 1
+    else:
+        odd += 1
+print(f"odd count :{odd}")
+print(f"even count :{even}")
