0dc89b5b225ca38db903abe27524590f16e473e4
gbuxss <jeevandhamala095@gmail.com>
2022-05-26 06:10:03 -0500

    added functions and code logic

--- core/exercises/fibonacci.py
+++ core/exercises/fibonacci.py
@@ -1,9 +1,18 @@
 # write a program to generate fibonacci series
+def fibonacci():
+    initial_num, second_num = 0, 1
+    # verify that n is greater than zero
+    n = int(input("Enter term to generate fibonacci series\n"))
+    if n <= 0:
+        print("Enter positive number!!")
+    elif n == 1:
+        print(initial_num)
+    else:
+        for num in range(n):
+            print(f"{initial_num}\t", end="")
+            third_num = initial_num + second_num
+            initial_num = second_num
+            second_num = third_num
 
-initial_num = 0
-second_num = 1
-for num in range ( 10 ) :
-    third_num = initial_num + second_num
-    initial_num = second_num
-    second_num = third_num
-    print()
+
+fibonacci()
