990009b19a2d7f9a3d1bab2278fcf6727402412d
gbuxss <jeevandhamala095@gmail.com>
2022-05-26 09:18:31 -0500

    calculator app modified

--- core/exercises/calculator.py
+++ core/exercises/calculator.py
@@ -3,7 +3,7 @@
 def user_input():
     first_number = float(input("enter first number:\t"))
     second_number = float(input("enter second number:\t"))
-    operation = input("Choose operation from list:\n Add \n Sub \n Mul \n Div\n Exit\n").lower()
+    operation = input("Choose operation from list:\n Add \n Sub \n Mul \n Div\n").lower()
 
     def perform_add(num1, num2):
         return num1 + num2
@@ -25,8 +25,6 @@ def user_input():
         print(perform_mul(first_number, second_number))
     elif operation == "Div".lower():
         print(perform_div(first_number, second_number))
-    elif operation == "Exit".lower():
-        break
     else:
         print("Wrong operation entered")
 
