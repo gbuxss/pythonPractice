d39e2f49867848355798eeb46da89f3d34e174b3
gbuxss <jeevandhamala095@gmail.com>
2022-05-26 14:53:09 -0500

    added function for reusuability

--- core/exercises/palindrome.py
+++ core/exercises/palindrome.py
@@ -1,8 +1,11 @@
 # Write a program to check input string is palindrome or not
+def palindrome(input_word):
+    reversed_word = input_word[::-1]
+    if user_input == reversed_word:
+        print(f"Word '{input_word}' is palindrome")
+    else:
+        print(f"Word '{input_word}' is not palindrome")
+
 
 user_input = input("Enter Word:\t")
-reversed_word = user_input[::-1]
-if user_input == reversed_word:
-    print("word is palindrome")
-else:
-    print("Word is not palindrome")
+palindrome(user_input)
